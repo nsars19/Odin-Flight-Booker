237c56d207ed409035bd3430645a37b8c80e8245
Nick <nsardonini@gmail.com>
2020-07-31 18:16:07 -0400

    add FlightsController

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,8 +1,4 @@
 class Flight < ApplicationRecord
-  belongs_to :from_airport, 
-              class_name: "Airport"
-              
-  belongs_to :to_airport, 
-              class_name: "Airport"
-              
+  belongs_to :from_airport, class_name: "Airport"   
+  belongs_to :to_airport, class_name: "Airport"
 end
