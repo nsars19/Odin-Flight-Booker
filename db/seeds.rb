75dcc10b949f4c5a42ec20db2ab8a85717172fd5
Nick <nsardonini@gmail.com>
2020-07-30 20:04:53 -0400

    update seed file to add Flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Airport.delete_all unless Airport.any?
+Flight.delete_all
+
+airports = [
+  {name: "Boston", code: "BOS"},
+  {name: "Los Angeles", code: "LAX"},
+  {name: "O'Hare", code: "ORD"},
+  {name: "Atlanta", code: "ATL"},
+  {name: "Dallas/Fort Worth", code: "DFW"},
+  {name: "Denver", code: "DEN"},
+  {name: "John F Kennedy", code: "JFK"},
+  {name: "San Francisco", code: "SFO"},
+  {name: "Seattle-Tacoma", code: "SEA"},
+  {name: "McCarran", code: "LAS"}
+]
+
+def random_date
+  Time.now + (rand(12) + 1).month
+end
+
+def random from = 21, to = 30
+  rand from..to
+end
+
+flights = []
+
+1000.times do
+  start = random_date
+  finish = start + random(2, 12).hour
+
+  flights << {start: start, 
+              finish: finish, 
+              from_airport_id: random, 
+              to_airport_id: random}
+end
+
+
+airports.each { |airport| Airport.create airport } unless Airport.any?
+flights.each { |flight| Flight.create flight }
