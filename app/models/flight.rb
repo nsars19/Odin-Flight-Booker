f52a547f948c8d059476276934c5836bed65227b
Nick <nsardonini@gmail.com>
2020-08-03 20:51:26 -0400

    add ::find_flights
    used to query for flights with given parameters selected by form on flight#index view.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,13 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, class_name: "Airport"   
   belongs_to :to_airport, class_name: "Airport"
+  
+  def self.find_flights form_params
+    from_id = form_params["from_airport"]["id"]
+    to_id   = form_params["to_airport"]["id"]
+    flights = Flight.where(from_airport_id: from_id, to_airport_id: to_id)
+    
+    date = "#{form_params["year"]}-#{form_params["month"]}-#{form_params["day"]}"
+    flights.filter { |flight| flight.start.to_date.to_s == date }
+    end
 end
