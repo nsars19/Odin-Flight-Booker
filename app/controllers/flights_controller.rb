016882fb5eb62499dee99d2dc436cb2adb2c0bec
Nick <nsardonini@gmail.com>
2020-08-03 20:50:27 -0400

    refactor #index. add strong params method

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,17 @@
 class FlightsController < ApplicationController
+  def index
+    @airports = Airport.select(:name, :id)   
+    @airport_options = @airports.map { |a| [a.name, a.id] }
+
+    @flights = Flight.find_flights
+  end
+
+  private
+
+  def flight_params
+    params.require(:flight).permit(:from_airport, 
+                                   :to_airport, 
+                                   :num_passengers,
+                                   :date)
+  end
 end
