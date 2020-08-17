40d670c416c63bc231b89887e4ba3509ced3d8e5
Nick <nsardonini@gmail.com>
2020-08-17 16:05:09 -0400

    add scopes

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,12 +2,7 @@ class Flight < ApplicationRecord
   belongs_to :from_airport, class_name: "Airport"   
   belongs_to :to_airport, class_name: "Airport"
   
-  def self.find_flights form_params
-    from_id = form_params["from_airport"]["id"]
-    to_id   = form_params["to_airport"]["id"]
-    flights = Flight.where(from_airport_id: from_id, to_airport_id: to_id)
-    
-    date = "#{form_params["year"]}-#{form_params["month"]}-#{form_params["day"]}"
-    flights.filter { |flight| flight.start.to_date.to_s == date }
-    end
+  scope :on_date, ->(date) { where("flight_date = ?", date) }
+  scope :coming_from, ->(from_id) { where("from_airport_id = ?", from_id) }
+  scope :going_to, ->(to_id) { where("to_airport_id = ?", to_id) }
 end
