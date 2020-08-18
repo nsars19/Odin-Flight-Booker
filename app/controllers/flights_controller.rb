b4ecc2e7c40d4654f97cb41f2ed39f03ffbce7b6
Nick <nsardonini@gmail.com>
2020-08-18 15:13:28 -0400

    add activerecord queries to instance variables. add formatting method for dates

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,17 +1,29 @@
-class FlightsController < ApplicationController
+class FlightsController < ApplicationController  
   def index
-    @airports = Airport.select(:name, :id)   
-    @airport_options = @airports.map { |a| [a.name, a.id] }
+    @flights = Flight.all
+    @years = @flights.map { |f| f.flight_date.year }.uniq.sort
+    @days = @flights.map { |f| prepend_zero f.flight_date.day }.uniq.sort
+    
+    @months = @flights.map do |f|
+      [Date::MONTHNAMES[f.flight_date.month], prepend_zero(f.flight_date.month)]
+    end.uniq.sort { |a, b| a[1] <=> b[1] }
 
-    @flights = Flight.find_flights
+    @available_flights = 
+      Flight.where(from_airport_id: params[:from_airport_id],
+                     to_airport_id: params[:to_airport_id])
+            .on_date("#{params[:flight_year]}-#{
+                        params[:flight_month]}-#{
+                        params[:flight_day]}")
+                              
+
+    @airports = Airport.all.map { |a| [a.name, a.id] }
   end
 
   private
+  
+  def prepend_zero nums
+    return nums.map { |i| prepend_zero i } if nums.is_a? Array
 
-  def flight_params
-    params.require(:flight).permit(:from_airport, 
-                                   :to_airport, 
-                                   :num_passengers,
-                                   :date)
+    nums.to_s.length == 1 ? nums = "0#{nums}" : nums.to_s
   end
 end
