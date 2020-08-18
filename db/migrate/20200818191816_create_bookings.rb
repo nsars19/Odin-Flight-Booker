e08cc8ad40b1289c065c1a4c19f8ec43fad60e5d
Nick <nsardonini@gmail.com>
2020-08-18 18:51:40 -0400

    create passengers and bookings tables

--- db/migrate/20200818191816_create_bookings.rb
+++ db/migrate/20200818191816_create_bookings.rb
@@ -1,7 +1,9 @@
 class CreateBookings < ActiveRecord::Migration[5.2]
   def change
     create_table :bookings do |t|
-
+      t.references :flight
+      t.references :passenger
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_225104) do
+ActiveRecord::Schema.define(version: 2020_08_18_191823) do
 
   create_table "airports", force: :cascade do |t|
     t.string "name"
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id"
+    t.integer "passenger_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
+    t.index ["passenger_id"], name: "index_bookings_on_passenger_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -30,6 +39,13 @@ ActiveRecord::Schema.define(version: 2020_08_10_225104) do
     t.string "arrival_time"
     t.index ["from_airport_id"], name: "index_flights_on_from_airport_id"
     t.index ["to_airport_id"], name: "index_flights_on_to_airport_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
