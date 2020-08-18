21693ad5220c7d29c0983459a920ad48337581d3
Nick <nsardonini@gmail.com>
2020-08-18 19:46:54 -0400

    add associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, class_name: "Airport"   
   belongs_to :to_airport, class_name: "Airport"
+  has_many :bookings
+  has_many :passengers, through: :bookings
   
   scope :on_date, ->(date) { where("flight_date = ?", date) }
   scope :coming_from, ->(from_id) { where("from_airport_id = ?", from_id) }
