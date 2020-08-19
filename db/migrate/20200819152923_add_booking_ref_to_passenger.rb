11aabfd29d58b2d760f0f43fb4b86d57c69e0cb4
Nick <nsardonini@gmail.com>
2020-08-19 12:25:32 -0400

    update db schema
    remove passenger reference from booking model & add booking reference to passenger model

---
+++ db/migrate/20200819152923_add_booking_ref_to_passenger.rb
@@ -0,0 +1,5 @@
+class AddBookingRefToPassenger < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :passengers, :booking, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_191823) do
+ActiveRecord::Schema.define(version: 2020_08_19_153012) do
 
   create_table "airports", force: :cascade do |t|
     t.string "name"
@@ -21,11 +21,9 @@ ActiveRecord::Schema.define(version: 2020_08_18_191823) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "flight_id"
-    t.integer "passenger_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["flight_id"], name: "index_bookings_on_flight_id"
-    t.index ["passenger_id"], name: "index_bookings_on_passenger_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -46,6 +44,8 @@ ActiveRecord::Schema.define(version: 2020_08_18_191823) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_id"
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
 end
