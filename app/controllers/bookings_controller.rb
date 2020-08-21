12e1368a484fde4a9ea54ada8a6520cc216169e9
Nick <nsardonini@gmail.com>
2020-08-21 17:02:11 -0400

    add #create, #show, and strong params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,4 +4,27 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     params[:num_passengers].to_i.times { @booking.passengers.build }
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render new_booking_path
+    end
+  end
+
+  def show
+    @booking = Booking.find params[:id]
+    @flight = @booking.flight
+    @passengers = @booking.passengers
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, :num_passengers,
+      passengers_attributes: [:id, :name, :email])
+  end
 end
